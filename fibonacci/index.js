498c2774e865d3cbed49e2e300b8a455db56777f
Stephen-Roach <stephenroach21@gmail.com>
2021-02-03 18:24:17 -0600

    added jest testing for fib challenge

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -1,12 +1,14 @@
-//Fibonacci Series
+// Fibonacci Series
 // A series of numbers in which each number (Fibonacci Number) is the sum of the two preceding numbers.
 // The simplest is the series 1, 1, 2, 3, 5, 8, etc.
 
-//Recursive Solution
+// Recursive Solution
 
-function fibonacci(n) {
+function fib(n) {
   if (n < 2) {
     return n;
   }
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  return fib(n - 1) + fib(n - 2);
 }
+
+module.exports = fib;
