b50f2714b44b20ea566a019ff553adb09d73b31f
Stephen-Roach <stephenroach21@gmail.com>
2021-02-09 19:15:33 -0600

    added another solution

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -2,11 +2,28 @@
 // A series of numbers in which each number (Fibonacci Number) is the sum of the two preceding numbers.
 // The simplest is the series 1, 1, 2, 3, 5, 8, etc.
 
+function fibonacciGenerator(n) {
+  let output = [];
+  if (n === 1) {
+    output = [0];
+  } else if (n === 2) {
+    output = [0, 1];
+  } else {
+    output = [0, 1];
+    for (let i = 2; i < n; i++) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
+    }
+  }
+  return output;
+}
+
+console.log(fibonacciGenerator(5));
+
 //Recursive Solution
 
-function fibonacci(n) {
-  if (n < 2) {
-    return n;
-  }
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
+// function fibonacci(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
